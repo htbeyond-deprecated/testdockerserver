106447eb559be3b1380d51a45199f603cc0de8e0
sukyology <chingoo@hanmail.net>
2017-03-02 15:40:27 +0900

    first commit

---
+++ testdockerserver/src/main/java/com/example/HomeController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by SukkyoSuh on 2017-03-02.
+ */
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(value="/")
+    public String home(){
+	return "server";
+    }
+
+}
